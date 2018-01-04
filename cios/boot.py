b62c684159a7d2c2dec64cf4e22d621961fd6f10
code <code@atercap.com>
2018-01-04 00:11:46 -0500

    Define bootstrap()

--- cios/boot.py
+++ cios/boot.py
@@ -5,10 +5,23 @@
 bootstrap() is the main function, and is called as soon as the module is loaded."""
 
 import stackless
+import importlib
 import sys
 import os
 
+from .config import config
 import .util
 
-def bootstrap():
-  pass
+def bootstrap(*args, **kwargs):
+  loc = config["Boot"].get("ModuleLocation", "/etc/cios.d/")
+  sys.path.append(loc)
+  
+  modules = []
+  
+  for name in os.listdir(loc):
+    if name.endswith(".py"):
+      module = importlib.import_module( module[:-3] )
+      module.bootstrap(*args, **kwargs)
+      modules.append(module)
+  
+  stackless.run()
